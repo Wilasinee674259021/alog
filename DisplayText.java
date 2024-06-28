7cd27cc41c373de2550745a32a0d631799b24331
Wilasinee <naruapon@gmail.com>
2024-06-28 10:49:12 +0700

    first commit

---
+++ DisplayText.java
@@ -0,0 +1,8 @@
+public class DisplayText {
+    public static void main(String[] args) {
+        System.out.println("Wilasinee"+"Janthanasot");
+        System.out.println(2+1);
+        system.out.println("NPRU"+2567);
+        Sysem.out.println(0+"6"+9);
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    Public static void main(Static[] args) {
+        System.out.println("HelloWorld");
+    }
+}
